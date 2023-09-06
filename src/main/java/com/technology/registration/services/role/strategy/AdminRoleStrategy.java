89c7cc0741a755e11f587bc59e40f74e1ab1a10e
f103gt <z5155082@gmail.com>
2023-09-06 18:04:03 +0300

    modified strategy pattern for role modifications

--- src/main/java/com/technology/registration/services/role/strategy/AdminRoleStrategy.java
+++ src/main/java/com/technology/registration/services/role/strategy/AdminRoleStrategy.java
@@ -1,41 +1,11 @@
 package com.technology.registration.services.role.strategy;
 
-import com.technology.registration.models.Role;
-import com.technology.registration.models.User;
-import com.technology.registration.repositories.UserRepository;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.Set;
-
-public class AdminRoleStrategy implements RoleStrategy {
+@Service
+public class AdminRoleStrategy extends AbstractRoleStrategy {
     @Override
-    public void addRole(UserRepository userRepository, String username) {
-        Optional<User> userOptional = userRepository.findUserByEmail(username);
-        userOptional.ifPresentOrElse(user -> {
-                    Role role = new Role();
-                    role.setRoleName("ADMIN");
-                    user.getRoles().add(role);
-                    userRepository.save(user);
-                },
-                () -> {
-                    throw new UsernameNotFoundException("User with email " + username + "does not exist");
-                });
-    }
-
-    @Override
-    public void deleteRole(UserRepository userRepository, String username) {
-        Optional<User> userOptional = userRepository.findUserByEmail(username);
-        userOptional.ifPresentOrElse(user -> {
-                    String currenUserName = getCurrenUserName();
-                    if (!user.getEmail().equals(currenUserName)) {
-                        Set<Role> roles = user.getRoles();
-                        roles.removeIf(role -> role.getRoleName().equals("ADMIN"));
-                        userRepository.save(user);
-                    }
-                },
-                () -> {
-                    throw new UsernameNotFoundException("User with email " + username + "does not exist");
-                });
+    public String getRoleName() {
+        return "ADMIN";
     }
 }
