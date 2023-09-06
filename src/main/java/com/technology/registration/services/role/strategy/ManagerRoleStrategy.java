f1d87f92cfe030068e8a87fc7bb573b6aee90c55
f103gt <z5155082@gmail.com>
2023-09-06 18:04:03 +0300

    modified strategy pattern for role modifications

--- src/main/java/com/technology/registration/services/role/strategy/ManagerRoleStrategy.java
+++ src/main/java/com/technology/registration/services/role/strategy/ManagerRoleStrategy.java
@@ -1,44 +1,11 @@
 package com.technology.registration.services.role.strategy;
 
-import com.technology.registration.models.Role;
-import com.technology.registration.models.User;
-import com.technology.registration.repositories.UserRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.Set;
-
-public class ManagerRoleStrategy implements RoleStrategy {
+@Service
+public class ManagerRoleStrategy extends AbstractRoleStrategy {
     @Override
-    @Transactional
-    public void addRole(UserRepository userRepository, String username) {
-        Optional<User> userOptional = userRepository.findUserByEmail(username);
-        userOptional.ifPresentOrElse(user -> {
-                    Role role = new Role();
-                    role.setRoleName("MANAGER");
-                    user.getRoles().add(role);
-                    userRepository.save(user);
-                },
-                () -> {
-                    throw new UsernameNotFoundException("User with email " + username + "does not exist");
-                });
-    }
-
-    @Override
-    @Transactional
-    public void deleteRole(UserRepository userRepository, String username) {
-        Optional<User> userOptional = userRepository.findUserByEmail(username);
-        userOptional.ifPresentOrElse(user -> {
-                    String currenUserName = getCurrenUserName();
-                    if(!user.getEmail().equals(currenUserName)){
-                        Set<Role> roles = user.getRoles();
-                        roles.removeIf(role -> role.getRoleName().equals("MANAGER"));
-                        userRepository.save(user);
-                    }
-                },
-                () -> {
-                    throw new UsernameNotFoundException("User with email " + username + "does not exist");
-                });
+    public String getRoleName() {
+        return "MANAGER";
     }
 }
