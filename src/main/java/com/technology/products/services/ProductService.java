73a2f109c0a05dc5992921678745916ce307f030
f103gt <z5155082@gmail.com>
2023-09-11 12:58:15 +0300

    get all the products

--- src/main/java/com/technology/products/services/ProductService.java
+++ src/main/java/com/technology/products/services/ProductService.java
@@ -1,9 +1,15 @@
 package com.technology.products.services;
 
+import com.technology.products.ProductDto;
+import com.technology.products.models.Product;
 import com.technology.products.registration.request.ProductRegistrationRequest;
+
+import java.util.List;
 
 public interface ProductService {
     void saveProduct(ProductRegistrationRequest productRegistrationRequest);
 
     void deleteProduct(String productName);
+
+    List<ProductDto> getAllProducts();
 }
--- src/main/java/com/technology/products/services/ProductServiceImpl.java
+++ src/main/java/com/technology/products/services/ProductServiceImpl.java
@@ -3,7 +3,7 @@ package com.technology.products.services;
 import com.technology.category.exceptions.CategoryNotFoundException;
 import com.technology.category.models.Category;
 import com.technology.category.repositories.CategoryRepository;
-import com.technology.exception.general.exceptions.ObjectNotFoundException;
+import com.technology.products.ProductDto;
 import com.technology.products.exceptions.ProductNotFoundException;
 import com.technology.products.exceptions.ProductObjectAlreadyExistsException;
 import com.technology.products.models.Product;
@@ -13,7 +13,9 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -53,9 +55,39 @@ public class ProductServiceImpl implements ProductService {
     @Override
     @Transactional
     public void deleteProduct(String productName) {
-        if(productRepository.findProductByProductName(productName).isEmpty()){
-            throw new ProductNotFoundException("Product "+productName+" not found");
+        if (productRepository.findProductByProductName(productName).isEmpty()) {
+            throw new ProductNotFoundException("Product " + productName + " not found");
         }
         productRepository.deleteProductByProductName(productName);
+    }
+
+    //return products sorted by product category, product name,product quantitry
+    @Override
+    @Transactional
+    public List<ProductDto> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(product -> new ProductDto(
+                        product.getCategory().getCategoryName(),
+                        product.getProductName(),
+                        product.getSku(),
+                        product.getQuantity(),
+                        product.getPrice()
+                ))
+                .sorted((productDto1, productDto2) ->
+                {
+                    int categoryComparison = productDto1.getCategoryName()
+                            .compareToIgnoreCase(productDto2.getCategoryName());
+                    if (categoryComparison == 0) {
+                        int quantityComparison = Integer.compare(
+                                productDto1.getQuantity(), productDto2.getQuantity());
+                        if (quantityComparison == 0) {
+                            return productDto1.getProductName()
+                                    .compareToIgnoreCase(productDto2.getProductName());
+                        }
+                        return quantityComparison;
+                    }
+                    return categoryComparison;
+                })
+                .collect(Collectors.toList());
     }
 }
