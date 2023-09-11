cc571475a05ad89d59ec74c161d7b469d00a1576
f103gt <z5155082@gmail.com>
2023-09-11 12:58:15 +0300

    get all the products

---
+++ src/main/java/com/technology/products/ProductDto.java
@@ -0,0 +1,18 @@
+package com.technology.products;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Getter
+public class ProductDto {
+
+    private String categoryName;
+    private String productName;
+
+    private String sku;
+    private Integer quantity;
+    private BigDecimal price;
+}
