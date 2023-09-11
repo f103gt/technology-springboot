68db9350243b8fde4ef65bcd940089c97b794287
f103gt <z5155082@gmail.com>
2023-09-11 15:11:59 +0300

    in all the controllers modified methods that return list of all the objects to return respose entity wiht this list

--- src/main/java/com/technology/management/ProductManagementController.java
+++ src/main/java/com/technology/management/ProductManagementController.java
@@ -1,12 +1,17 @@
 package com.technology.management;
 
+import com.technology.products.ProductDto;
+import com.technology.products.models.Product;
 import com.technology.products.registration.request.ProductRegistrationRequest;
 import com.technology.products.services.ProductService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductManagementController {
@@ -18,8 +23,10 @@ public class ProductManagementController {
     }
 
     @GetMapping("/manager/all-products")
-    public ResponseEntity<String> allProducts() {
-        return ResponseEntity.ok("A new category was successfully added");
+    public ResponseEntity<List<ProductDto>> allProducts() {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(productService.getAllProducts());
     }
 
     //TODO during the process of adding new product pass the product name to
