8c12311411a129aaf76eabb84bc420ed3ec9a801
f103gt <z5155082@gmail.com>
2023-09-14 14:06:19 +0300

    implemented testing of the app's behavior in case of an attempt to save a category that already exists

--- src/test/java/com/technology/category/services/CategoryServiceImplTest.java
+++ src/test/java/com/technology/category/services/CategoryServiceImplTest.java
@@ -1,20 +1,19 @@
 package com.technology.category.services;
 
+import com.technology.category.exceptions.CategoryAlreadyExistsException;
+import com.technology.category.exceptions.ParentCategoryNotFoundException;
 import com.technology.category.models.Category;
 import com.technology.category.registration.request.CategoryRegistrationRequest;
 import com.technology.category.repositories.CategoryRepository;
 import com.technology.products.repositories.ProductRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,21 +36,57 @@ class CategoryServiceImplTest {
     }
 
     @Test
-    void saveValidCategoryInputWithNoParentCategory() {
-        //Arrange
+    void saveCategory_SavesCategory_WhenCategoryIsValidAndHasNoParentCategory() {
+        //arrange
         CategoryRegistrationRequest request = new CategoryRegistrationRequest();
         request.setParentCategoryName(null);
         request.setCategoryName("TestCategory");
         //act
         when(categoryRepository
-                .findCategoryByCategoryName(anyString()))
+                .findCategoryByCategoryName(request.getCategoryName()))
                 .thenReturn(Optional.empty());
         categoryService.saveCategory(request);
 
         //assert
         verify(categoryRepository, times(1)).save(any(Category.class));
     }
-    
+
+    @Test
+    void saveCategory_SavesCategory_WhenCategoryIsValidAndHasParentCategory() {
+        //arrange
+        CategoryRegistrationRequest request = new CategoryRegistrationRequest();
+        request.setParentCategoryName("ParentTestCategory");
+        request.setCategoryName("TestCategory");
+        //act
+        when(categoryRepository
+                .findCategoryByCategoryName(request.getCategoryName()))
+                .thenReturn(Optional.empty());
+        when(categoryRepository
+                .findCategoryByCategoryName(request.getParentCategoryName()))
+                .thenReturn(Optional.of(new Category()));
+
+        categoryService.saveCategory(request);
+
+        //assert
+        verify(categoryRepository, times(1)).save(any(Category.class));
+    }
+
+
+    @Test
+    void saveCategory_Throws_CategoryAlreadyExistsException() {
+        //arrange
+        CategoryRegistrationRequest request = new CategoryRegistrationRequest();
+        request.setCategoryName("TestCategory");
+
+        when(categoryRepository
+                .findCategoryByCategoryName(request.getCategoryName()))
+                .thenReturn((Optional.of(new Category())));
+        //act and assert
+        assertThrows(CategoryAlreadyExistsException.class,
+                () -> categoryService.saveCategory(request),
+                "Category " + request.getCategoryName() + " already exists");
+    }
+
 
     @Test
     void deleteCategory() {
