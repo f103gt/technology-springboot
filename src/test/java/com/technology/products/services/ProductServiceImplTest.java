64dbab9f3f44d8533f3f4654a33118aff4a0d199
f103gt <z5155082@gmail.com>
2023-09-16 00:52:36 +0300

    added yet not implemented products unit tests

---
+++ src/test/java/com/technology/products/services/ProductServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.technology.products.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceImplTest {
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+}
