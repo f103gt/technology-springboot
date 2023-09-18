6b6f06dd60cd39afae87effdde7cdc7484ef29b3
f103gt <z5155082@gmail.com>
2023-09-18 15:08:32 +0300

    successfully save new produt unit test

--- src/main/java/com/technology/products/registration/request/ProductRegistrationRequest.java
+++ src/main/java/com/technology/products/registration/request/ProductRegistrationRequest.java
@@ -3,6 +3,7 @@ package com.technology.products.registration.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 @NotNull
 @NotEmpty
 @NotBlank
+@AllArgsConstructor
 public class ProductRegistrationRequest {
     private String categoryName;
     private String productName;
--- src/test/java/com/technology/products/services/ProductServiceImplTest.java
+++ src/test/java/com/technology/products/services/ProductServiceImplTest.java
@@ -1,13 +1,69 @@
 package com.technology.products.services;
 
+import com.technology.category.models.Category;
+import com.technology.category.repositories.CategoryRepository;
+import com.technology.products.models.Product;
+import com.technology.products.registration.request.ProductRegistrationRequest;
+import com.technology.products.repositories.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.Optional;
 
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
+    @Mock
+    protected ProductRepository productRepository;
+
+    @Mock
+    protected CategoryRepository categoryRepository;
+
+    @InjectMocks
+    protected ProductServiceImpl productService;
+
+    private ProductRegistrationRequest request;
+
+    @BeforeEach
+    void setUp() {
+        request =
+                new ProductRegistrationRequest(
+                        "Category",
+                        "Product 1",
+                        "SKU1",
+                        1,
+                        BigDecimal.TEN
+                );
+    }
 
     @Test
-    void saveProduct() {
+    void saveProduct_SavesProduct() {
+        //arrange setUp
+
+        //act
+        when(categoryRepository.findCategoryByCategoryName(
+                request.getCategoryName()
+        )).thenReturn(Optional.of(new Category()));
+
+        when(productRepository.findProductByProductName(
+                request.getProductName()
+        )).thenReturn(Optional.empty());
+
+        productService.saveProduct(request);
+
+        //assert
+        verify(productRepository,times(1))
+                .save(any(Product.class));
+    }
+
+    @Test
+    void saveProduct_Throws_CategoryNotFoundException() {
     }
 
     @Test
