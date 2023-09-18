e4148234388fe911bf2c4e68539a352fc135129e
f103gt <z5155082@gmail.com>
2023-09-18 20:07:22 +0300

    get all products unit test

--- src/test/java/com/technology/products/services/ProductServiceImplGetAllProductsTest.java
+++ src/test/java/com/technology/products/services/ProductServiceImplGetAllProductsTest.java
@@ -1,10 +1,72 @@
 package com.technology.products.services;
 
+import com.technology.category.models.Category;
+import com.technology.products.ProductDto;
+import com.technology.products.models.Product;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 public class ProductServiceImplGetAllProductsTest extends ProductServiceImplTest {
 
     @Test
     void getAllProducts() {
+        //arrange
+        Category category = Category.builder()
+                .categoryName("Test Category")
+                .build();
+        List<Product> products = List.of(
+                Product.builder()
+                        .id(BigInteger.ONE)
+                        .category(category)
+                        .productName("Test Product 1")
+                        .sku("SKU1")
+                        .quantity(1)
+                        .price(BigDecimal.TEN)
+                        .build(),
+                Product.builder()
+                        .id(BigInteger.TWO)
+                        .category(category)
+                        .productName("Test Product 2")
+                        .sku("SKU2")
+                        .quantity(1)
+                        .price(BigDecimal.TEN)
+                        .build(),
+                Product.builder()
+                        .id(BigInteger.TEN)
+                        .category(Category.builder()
+                                .categoryName("Test Category 1")
+                                .build())
+                        .productName("Test Product 3")
+                        .sku("SKU10")
+                        .quantity(1)
+                        .price(BigDecimal.TEN)
+                        .build()
+
+        );
+
+
+        //act
+        when(productRepository.findAll()).thenReturn(products);
+
+        List<ProductDto> productDtos = productService.getAllProducts();
+
+        //assert
+        IntStream.range(0, products.size())
+                .forEach(index -> {
+                    Product product = products.get(index);
+                    ProductDto productDto = productDtos.get(index);
+                    assertThat(productDto)
+                            .extracting(ProductDto::getCategoryName, ProductDto::getProductName)
+                            .containsExactly(product.getCategory().getCategoryName(), product.getProductName());
+                });
     }
 }
--- src/test/java/com/technology/products/services/ProductServiceImplTest.java
+++ src/test/java/com/technology/products/services/ProductServiceImplTest.java
@@ -1,25 +1,11 @@
 package com.technology.products.services;
 
-import com.technology.category.exceptions.CategoryNotFoundException;
-import com.technology.category.models.Category;
 import com.technology.category.repositories.CategoryRepository;
-import com.technology.products.exceptions.ProductObjectAlreadyExistsException;
-import com.technology.products.models.Product;
-import com.technology.products.registration.request.ProductRegistrationRequest;
 import com.technology.products.repositories.ProductRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.parameters.P;
-
-import java.math.BigDecimal;
-import java.util.Optional;
-
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
