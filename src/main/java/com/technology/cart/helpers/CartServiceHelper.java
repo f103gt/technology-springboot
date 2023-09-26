c17d047639b570f16bb457eb5385f1fbdc550560
f103gt <z5155082@gmail.com>
2023-09-26 21:04:42 +0300

    in CartServiceHelper added method to find a particular cartItem

--- src/main/java/com/technology/cart/helpers/CartServiceHelper.java
+++ src/main/java/com/technology/cart/helpers/CartServiceHelper.java
@@ -2,6 +2,7 @@ package com.technology.cart.helpers;
 
 import com.technology.cart.factories.CartFactory;
 import com.technology.cart.models.Cart;
+import com.technology.cart.models.CartItem;
 import com.technology.cart.repositories.CartRepository;
 import com.technology.registration.models.User;
 import com.technology.registration.repositories.UserRepository;
@@ -9,6 +10,11 @@ import com.technology.security.adapters.SecurityUser;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class CartServiceHelper {
     public static User getUserFromContext(UserRepository userRepository) {
@@ -29,5 +35,11 @@ public class CartServiceHelper {
         return cart;
     }
 
-    public static void remove
+    public static Optional<CartItem> findParticularCartItem(Cart cart, BigInteger productId){
+        Set<CartItem> cartItems = new HashSet<>(cart.getCartItems());
+        return cartItems.stream()
+                .filter(cartItem ->
+                        cartItem.getProduct().getId().equals(productId))
+                .findFirst();
+    }
 }
