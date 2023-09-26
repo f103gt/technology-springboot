7606bad50e54ff1060e807510e2e2f55e7cbf3ab
f103gt <z5155082@gmail.com>
2023-09-26 18:53:07 +0300

    CartFactory created

---
+++ src/main/java/com/technology/cart/factories/CartFactory.java
@@ -0,0 +1,4 @@
+package com.technology.cart.factories;
+
+public class CartFactory {
+}
--- src/main/java/com/technology/cart/services/CartService.java
+++ src/main/java/com/technology/cart/services/CartService.java
@@ -11,6 +11,14 @@ public interface CartService {
      * cart exists or not, cart purification/removal
      *  will be implemented as soon as the order
      * is successfully made */
+
+    /* deleteCart method does not actually delete
+    * all the items from the cart
+    * only the relation between cart and use
+    * is being destroyed, and, as a replacement
+    * of it, a new relation between user order
+    * and the cart is being begeted.
+    * This approach helps to presume user oder history */
     //TODO if needed modify code to accept a parameter
     void deleteCart();
 }
