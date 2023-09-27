6e8155fa34311f4baddad4b33e00507e9ee7a098
f103gt <z5155082@gmail.com>
2023-09-27 21:04:56 +0300

    create and implement compareProductDtos in ProductServiceHelper

--- src/main/java/com/technology/product/helpers/ProductServiceHelper.java
+++ src/main/java/com/technology/product/helpers/ProductServiceHelper.java
@@ -1,4 +1,20 @@
 package com.technology.product.helpers;
 
+import com.technology.product.dto.ProductDto;
+
 public class ProductServiceHelper {
+    public static int compareProductDtos(ProductDto productDto1, ProductDto productDto2){
+        int categoryComparison = productDto1.getCategoryName()
+                .compareToIgnoreCase(productDto2.getCategoryName());
+        if (categoryComparison == 0) {
+            int quantityComparison = Integer.compare(
+                    productDto1.getQuantity(), productDto2.getQuantity());
+            if (quantityComparison == 0) {
+                return productDto1.getProductName()
+                        .compareToIgnoreCase(productDto2.getProductName());
+            }
+            return quantityComparison;
+        }
+        return categoryComparison;
+    }
 }
