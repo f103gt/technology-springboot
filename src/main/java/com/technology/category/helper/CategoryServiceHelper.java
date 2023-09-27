e32571ef6e1b785d011a1f0703c29322fa58c41c
f103gt <z5155082@gmail.com>
2023-09-27 13:50:28 +0300

    implement compareCategoryDtos in CategoryServiceHelper

--- src/main/java/com/technology/category/helper/CategoryServiceHelper.java
+++ src/main/java/com/technology/category/helper/CategoryServiceHelper.java
@@ -1,4 +1,16 @@
 package com.technology.category.helper;
 
+import com.technology.category.dto.CategoryDto;
+
 public class CategoryServiceHelper {
+    public static int compareCategoryDtos(CategoryDto categoryDto1, CategoryDto categoryDto2) {
+        int parentCategoriesComparison = categoryDto1.getParentCategoryName()
+                .compareToIgnoreCase(categoryDto2.getParentCategoryName());
+        if (parentCategoriesComparison == 0) {
+            return categoryDto1.getCategoryName()
+                    .compareToIgnoreCase(categoryDto2.getCategoryName());
+        }
+        return parentCategoriesComparison;
+    }
+
 }
