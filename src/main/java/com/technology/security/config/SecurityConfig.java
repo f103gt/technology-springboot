3622db7b9985492416e77d7ea6ac3fe862ac1fda
f103gt <z5155082@gmail.com>
2023-10-18 21:06:01 +0300

    refactored csrf protection configuration

--- src/main/java/com/technology/security/config/SecurityConfig.java
+++ src/main/java/com/technology/security/config/SecurityConfig.java
@@ -1,17 +1,14 @@
 package com.technology.security.config;
 
+import com.technology.security.csrf.CsrfCookieFilter;
+import com.technology.security.csrf.CustomCsrfTokenRequestHandler;
 import com.technology.security.jwt.JwtAuthenticationFilter;
 import com.technology.security.services.JpaUserDetailsService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -20,16 +17,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.*;
-import org.springframework.util.StringUtils;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.function.Supplier;
 
 @Configuration
 @EnableWebSecurity
@@ -70,7 +63,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(new CsrfCookieFilter(), UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(jwtAuthFilter,CsrfCookieFilter.class )
+                .addFilterAfter(jwtAuthFilter, CsrfCookieFilter.class)
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
@@ -92,33 +85,4 @@ public class SecurityConfig {
     }
 }
 
-final class CustomCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
-    private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
-
-    @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> deferredCsrfToken) {
-        this.delegate.handle(request, response, deferredCsrfToken);
-    }
-
-    @Override
-    public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
-        if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
-            return super.resolveCsrfTokenValue(request, csrfToken);
-        }
-        return this.delegate.resolveCsrfTokenValue(request, csrfToken);
-    }
-}
-
-final class CsrfCookieFilter extends OncePerRequestFilter {
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
-        // Render the token value to a cookie by causing the deferred token to be loaded
-        csrfToken.getToken();
-
-        filterChain.doFilter(request, response);
-    }
-}
 
